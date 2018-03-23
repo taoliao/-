ec547ff4980b0a05049c2de8d149db8ab0a86d53
LiaoTao <TL163.com>
2018-03-23 14:01:56 +0800

    初始化项目

---
+++ 列表倒计时/列表倒计时/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  列表倒计时
+//
+//  Created by corepress on 2018/3/23.
+//  Copyright © 2018年 corepress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 列表倒计时/列表倒计时/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  列表倒计时
+//
+//  Created by corepress on 2018/3/23.
+//  Copyright © 2018年 corepress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
